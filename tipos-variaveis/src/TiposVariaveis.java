0e2b2bbeeabe892c09adcb1cc2506e1417e2afd6
Adriano Ribeiro <adrianorm16@gmail.com>
2024-05-19 17:22:01 -0300

    Variaveis constantes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,6 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        //Tipos primitivos
         byte idade = 123;
         short ano = 2021;
         int cep = 21070333;
@@ -12,5 +13,8 @@ public class TiposVariaveis {
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal; //Casting
 
+
+        final double VALOR_DE_PI = 3.14;  //Variavel Constante, usando final e caixa alta
+        VALOR_DE_PI = 10.77;
     }
 }
