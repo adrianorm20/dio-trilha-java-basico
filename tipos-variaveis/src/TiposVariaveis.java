06a2462c3acbd1ebc0aa391e0656177a09eae2a1
Adriano Ribeiro <adrianorm16@gmail.com>
2024-05-19 17:17:12 -0300

    Tipos variaveis e Casting

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 98765432109L
+        float pi = 3.14F;
+        double = salario = 1275.33
+    
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //Casting
+
     }
 }
