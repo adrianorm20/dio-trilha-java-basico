c10d0cd7e4c686135dface196575fb9da63db52c
Adriano Ribeiro <adrianorm16@gmail.com>
2024-05-19 17:37:47 -0300

    Operadores concatenacao

---
+++ operadores/src/Operadores.java
@@ -0,0 +1,27 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
+
+
+        //Debbugar codigo abaixo
+        String concatenacao = "?";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+    }
+}
