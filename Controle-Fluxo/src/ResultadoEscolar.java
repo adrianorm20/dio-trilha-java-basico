170511d670a99e8d72930973ec2dfceffec6c25c
Adriano Ribeiro <adrianorm16@gmail.com>
2024-05-29 16:01:43 -0300

    Condicional Ternária

--- Controle-Fluxo/src/ResultadoEscolar.java
+++ Controle-Fluxo/src/ResultadoEscolar.java
@@ -1,13 +1,11 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 6;
+        //Condicional Ternária
+       /* String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);*/ 
 
-        if(nota >= 7){  //Condicional Encadeada
-            System.out.println("Aprovado");   
-        }
-        else if(nota >= 5 && nota < 7){
-            System.out.println("Prova Recuperação");
-        }else
-        System.out.println("Reprovado");
+        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
     }
 }
