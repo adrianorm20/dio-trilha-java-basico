d1a08aacaa79c6d689a205f2d56fa880d4c2de9b
Adriano Ribeiro <adrianorm16@gmail.com>
2024-05-29 15:49:09 -0300

    Condicional Composta

--- Controle-Fluxo/src/CaixaEletronico.java
+++ Controle-Fluxo/src/CaixaEletronico.java
@@ -1,7 +1,7 @@
 public class CaixaEletronico {
     
     public static void main(String[] args) {
-        double saldo = 25.0;
+     /* double saldo = 25.0;
         double valorSolicitado = 17.0;
 
         if(valorSolicitado < saldo){  //Condicional Simples
@@ -9,5 +9,17 @@ public class CaixaEletronico {
         }
                 
         System.out.println(saldo);
+    }
+ */   
+
+        double saldo = 25.0;
+        double valorSolicitado = 22.0;
+
+        if(valorSolicitado < saldo){ //Condicional Composta
+            saldo = saldo - valorSolicitado;
+            System.out.println("Novo saldo: " + saldo);
+        }  
+        else
+            System.out.println("Saldo insuficiente");
     }
 }
