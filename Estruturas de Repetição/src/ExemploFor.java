789a27fbd334d5d200710cbea1ac57d7cf6b3fd8
Adriano Ribeiro <adrianorm16@gmail.com>
2024-06-07 13:34:31 -0300

    Estrutura de Repetição "For"

---
+++ Estruturas de Repetição/src/ExemploFor.java
@@ -0,0 +1,8 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for ( int x = 1; x <= 20; x ++){
+            System.out.println("Contador: " + x);
+        }
+        System.out.println("Acabou a contagem.");
+    }
+}
